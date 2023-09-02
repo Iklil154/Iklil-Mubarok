6ef7881646f33276aefbd7715fc568653abf0e24
Iklil154 <143860592+Iklil154@users.noreply.github.com>
2023-09-02 11:03:50 +0700

    Add files via upload

---
+++ mashokk.py
@@ -0,0 +1,17 @@
+#variabel dan tipe data 
+
+x = 15
+y = 10
+surya = x + y
+print("hasil penjumlahan",surya)
+suki = x - y
+print("hasil pengurangan",suki)
+apaan = x * y
+print("hasil perkalian",apaan)
+wadu = x ** y
+print("hasil pangkat",wadu)
+woke = x / y
+print("hasil pembagian",woke)
+siti = x % y
+print("hasil modulus",siti)
+
